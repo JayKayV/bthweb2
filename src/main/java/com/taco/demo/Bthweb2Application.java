6768227d7fb4cdad5e5a96116ff727c327584916
JayKayV <accoezra@gmail.com>
2022-05-07 08:17:20 +0700

    Initial commit

---
+++ src/main/java/com/taco/demo/Bthweb2Application.java
@@ -0,0 +1,13 @@
+package com.taco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bthweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bthweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/taco/demo/Bthweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taco.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bthweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
